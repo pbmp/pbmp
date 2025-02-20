73f68c90a47d765e866dc9c3a0c2e2c5d52f2650
Christian yuda Pratama <53987991+christyuda@users.noreply.github.com>
2025-02-20 23:36:41 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
-  base: "/", 
+  base: "/pbmp.github.io/", 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
